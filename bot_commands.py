b1efa9c86b1edd0bf562f17b428282404be9decc
RyzeNGrind <vidurshan.sribalasuhabiramam@ontariotechu.net>
2020-06-10 05:29:49 +0000

    Attempt at fixing asyncio error for aioshoppy

--- bot_commands.py
+++ bot_commands.py
@@ -100,7 +100,7 @@ class Command(object):
         elif topic == "list":
             # text = print(await shoppy.query.list(page= 1))
             text = await shoppy.query.list(page=1)
-            return text
+            # return text
             await shoppy.close()
         else:
             text = "Unknown help topic!"
